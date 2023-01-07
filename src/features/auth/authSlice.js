ac82a7b7ff9c41f077473e3a3c13dc71720fb65b
cseswapon <cseswapon@gmail.com>
2023-01-08 01:40:45 +0600

    [ADD] - added to firebase login from

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 const initialState = {
   isLoading: false,
   isError: false,
@@ -6,6 +11,22 @@ const initialState = {
   email: "",
   role: "",
 };
+
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }) => {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  }
+);
+
+export const logInUser = createAsyncThunk(
+  "auth/logInUser",
+  async ({ email, password }) => {
+    // console.log(email,password);
+    return await signInWithEmailAndPassword(auth, email, password);
+  }
+);
+
 const authSlice = createSlice({
   name: "authSlice",
   initialState,
@@ -13,8 +34,49 @@ const authSlice = createSlice({
     singOut: (state) => {
       state.email = "";
     },
+    addEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    toggleLoading: (state) => {
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.email = "";
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.email = action.payload.user.email;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.isError = true;
+        state.isLoading = false;
+      })
+      .addCase(logInUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.email = "";
+      })
+      .addCase(logInUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.email = action.payload.user.email;
+        state.error = "";
+      })
+      .addCase(logInUser.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.isError = true;
+        state.isLoading = false;
+      });
   },
 });
 
-export const { singOut } = authSlice.actions;
-export default authSlice.reducer
+export const { singOut, addEmail, toggleLoading } = authSlice.actions;
+export default authSlice.reducer;
--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -1,34 +1,69 @@
 import { signOut } from "firebase/auth";
 import React from "react";
-
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-
+import { singOut } from "../../features/auth/authSlice";
+import auth from "../../firebase/firebase.config";
+import { useDispatch } from "react-redux";
 const Navbar = () => {
   const { pathname } = useLocation();
-
+  const auths = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const { isLoading, email } = auths;
+  // console.log(isLoading, email);
+  const handelLogout = () => {
+    signOut(auth).then(() => {
+      dispatch(singOut());
+    });
+  };
   return (
     <nav
       className={`h-14 fixed w-full z-[999] ${
         pathname === "/" ? null : "bg-white"
       }`}
     >
-      <ul className='max-w-7xl mx-auto flex gap-3 h-full items-center'>
-        <li className='flex-auto font-semibold text-2xl'>
-          <Link to='/'>JobBox</Link>
+      <ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
+        <li className="flex-auto font-semibold text-2xl">
+          <Link to="/">JobBox</Link>
         </li>
         <li>
-          <Link className='hover:text-primary' to='/jobs'>
+          <Link className="hover:text-primary" to="/jobs">
             Jobs
           </Link>
         </li>
 
         <li>
-          <Link
-            className='border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all '
-            to='/login'
-          >
-            Login
-          </Link>
+          {!isLoading && email &&
+            <span>
+              <Link
+                className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+                to="/dashboard"
+              >
+                Dashboard
+              </Link>
+              <Link
+                className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all mx-3"
+                to="/register"
+              >
+                register
+              </Link>
+            </span>
+          }
+          {!isLoading && email ? (
+            <button
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              onClick={() => handelLogout()}
+            >
+              LogOut
+            </button>
+          ) : (
+            <Link
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              to="/login"
+            >
+              Login
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
